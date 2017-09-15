8cfc5708d6e2329b8c953dfb8ac2f8cc0b13edfd
Andres Barradas <arbadev@gmail.com>
2017-09-15 19:08:40 -0400

    initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+      <li v-for='name in names' v-text='name'></li>
+    </ul>
+    <input v-model='newName'>
+    <button @click='addName'>add name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      names: ['putanga', 'tuki', 'cabeza'],
+      newName: '',
+    };
+  },
+  methods: {
+    addName() {
+      this.names.push(this.newName)
+      this.newName = ''
+    },
+  },
+};
+</script>
